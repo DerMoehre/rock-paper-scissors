16a2d798cd513dba8667403f7566419bbd6adbb8
DerMoehre <94791958+DerMoehre@users.noreply.github.com>
2023-11-05 10:22:50 +0100

    added scoring

--- rps.js
+++ rps.js
@@ -16,18 +16,29 @@ let playGame = (playerChoice, computerChoice) => {
     if (playerChoice == computerChoice){
         return "DRAW";
     } else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")){
+        scorePlayer += 1;
         return `You win! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
     } else {
+        scoreComputer += 1;
         return `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}`;
     }
 }
 
 let game = () => {
+
     for (let i = 0; i <= 4; i++) {
         console.log(playGame(playerChoice(), computerChoice()));
+        
     }
+    if (scorePlayer > scoreComputer) {
+        alert(`You WON! Final Score: ${scorePlayer} - ${scoreComputer}`);
+    } else if (scoreComputer > scorePlayer) {
+        alert(`You LOSE! Final Score: ${scoreComputer} - ${scorePlayer}`);
+    } else (alert(`DRAW! Final Score: ${scoreComputer} - ${scorePlayer}`));
 }
 
+let scorePlayer = 0;
+let scoreComputer = 0;
 game();
 
 
