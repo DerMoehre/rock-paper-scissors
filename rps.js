ce3cb13a28f4953ac302a96841abf941f82199cd
DerMoehre <94791958+DerMoehre@users.noreply.github.com>
2024-02-04 14:53:44 +0100

    added basic functionality of score

--- rps.js
+++ rps.js
@@ -1,3 +1,25 @@
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+const btnRock = document.querySelector('.rock');
+const btnPaper = document.querySelector('.paper');
+const btnScissors = document.querySelector('.scissors');
+const playerScore = document.querySelector('.player-score')
+const cpuScore = document.querySelector('.cpu-score')
+
+
+btnRock.addEventListener('click', () => {
+    playGame("rock", computerChoice())
+});
+
+btnPaper.addEventListener('click', () => {
+    playGame("paper", computerChoice())
+});
+
+btnScissors.addEventListener('click', () => {
+    playGame("scissors", computerChoice())
+});
+
 let computerChoice = () => {
     let choices = ["rock", "paper", "scissors"];
 
@@ -7,28 +29,34 @@ let computerChoice = () => {
     return choices[randomChoice];
 }
 
-let playerChoice = () => {
+/* let playerChoice = () => {
     let playerInput = prompt("Choose your weapon [rock, paper, scissors]");
     return playerInput.toLowerCase();
-}
+} */
 
 let playGame = (playerChoice, computerChoice) => {
     if (playerChoice == computerChoice){
-        return "DRAW";
-    } else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")){
+        scoreComputer += 1;
         scorePlayer += 1;
-        return `You win! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
+        playerScore.textContent = scorePlayer;
+        cpuScore.textContent = scoreComputer;
+        //return "DRAW";
+    } else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")){
+        scorePlayer += 1;       
+        playerScore.textContent = scorePlayer;
+        //return `You win! ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
     } else {
         scoreComputer += 1;
-        return `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}`;
+        cpuScore.textContent = scoreComputer;
+        //return `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}`;
     }
 }
 
 let game = () => {
 
-    for (let i = 0; i <= 4; i++) {
+/*     for (let i = 0; i <= 4; i++) {
         console.log(playGame(playerChoice(), computerChoice()));
-    }
+    } */
     // check final score
     if (scorePlayer > scoreComputer) {
         alert(`You WON! Final Score: ${scorePlayer} - ${scoreComputer}`);
@@ -37,8 +65,7 @@ let game = () => {
     } else (alert(`DRAW! Final Score: ${scoreComputer} - ${scorePlayer}`));
 }
 
-let scorePlayer = 0;
-let scoreComputer = 0;
+
 /* game(); */
 
 
